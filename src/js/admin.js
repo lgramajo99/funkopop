fbf6014f9eeed7689b8a06afde9ecf70e673c717
Luciano Gramajo <lucianongramajo@gmail.com>
2020-07-13 11:40:28 -0300

    se empezo a trabajar con js en el modal

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,3 +1,17 @@
 import '../css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap'
+import Funko from "./funko";
+
+let listaProducto = [];
+
+function agragarProductos() {
+    let codigo = document.getElementById("codigo").value,
+        nombre = document.getElementById("nombre").value,
+        nSerie = document.getElementById("nSerie").value,
+        descripcion = document.getElementById("descripcion").value,
+        categoria = document.getElementById("categoria").value,
+        imagen = document.getElementById("imagen").value
+
+    let Productos = new Funko(codigo, nombre, nSerie, categoria, descripcion, imagen);
+}
---
+++ src/js/funko.js
@@ -0,0 +1,12 @@
+export default class Funko {
+    constructor(codigo, nombre, nSerie, categoria, descripcion, imagen) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.nSerie = nSerie;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.estado = true;
+    }
+
+}
